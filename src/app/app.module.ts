252d8f6b4300f2ef9dc4203f25fe2fd6ebc5b99b
DanielFloria <daniflorialopez@gmail.com>
2022-09-13 12:44:54 +0200

    Personajes mostrados en tarjetas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CharactersComponent } from './characters/characters.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CharactersComponent,
+    AboutComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/characters/characters.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-characters',
+  templateUrl: './characters.component.html',
+  styleUrls: ['./characters.component.css']
+})
+export class CharactersComponent implements OnInit {
+
+  characters: any = null;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http
+    .get('https://rickandmortyapi.com/api/character/1,2,3,4,5,6,7,8')
+    .subscribe(
+      result => {
+        this.characters = result;
+      },
+      error => {
+        console.log('No funciona');
+      }
+    );
+  }
+
+}
